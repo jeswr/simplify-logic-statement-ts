129c8cd2d9a004344d114cbe9280dd05d92b7fc5
jeswr <jesse.wright@anu.edu.au>
2020-12-26 22:27:45 +1100

    WIP: Pushing negation back

--- lib/simplifiers/not.ts
+++ lib/simplifiers/not.ts
@@ -3,7 +3,63 @@ import {
 } from '../types';
 import { simplify } from './index';
 
-export function simplifyNot<T>(not: NotStatement<T>): LogicalStatementOutput<T> {
+export function simplifyNot<T>(not: NotStatement<T>, flatten = true): LogicalStatementOutput<T> {
+  if (flatten) {
+    const internalStatement = not.statement;
+
+    if (internalStatement.type === LogicalStatementType.and) {
+      const noter: NotStatement<T>[] = [
+        ...internalStatement.statement.and,
+        ...internalStatement.statement.not,
+        ...internalStatement.statement.or,
+        ...internalStatement.statement.xone,
+        ...internalStatement.statement.statement,
+      ].map(((statement) => ({
+        type: LogicalStatementType.not,
+        statement,
+      })));
+      return simplify({
+        type: LogicalStatementType.or,
+        statement: {
+          and: [],
+          not: noter,
+          or: [],
+          xone: [],
+          statement: [],
+        },
+      });
+    }
+
+    if (internalStatement.type === LogicalStatementType.or) {
+      const nots: NotStatement<T>[] = [
+        ...internalStatement.statement.and,
+        ...internalStatement.statement.not,
+        ...internalStatement.statement.or,
+        ...internalStatement.statement.xone,
+        ...internalStatement.statement.statement,
+      ].map(((statement) => ({
+        type: LogicalStatementType.not,
+        statement,
+      })));
+      return simplify({
+        type: LogicalStatementType.and,
+        statement: {
+          and: [],
+          not: nots,
+          or: [],
+          xone: [],
+          statement: [],
+        },
+      });
+    }
+  }
+
+  // if (not.statement.statement === LogicalStatementType.xone) {
+  //   return {
+
+  //   }
+  // }
+
   const statement = simplify(not.statement);
 
   // Double 'not' so they cancel each other out
